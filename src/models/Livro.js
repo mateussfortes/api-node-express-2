96002105df1b0573a1fb6ac08ebd5b6c8a76063a
mateussfortes <mateussilveirafortes@gmail.com>
2024-01-04 15:52:22 -0300

    Validando os dados

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -14,9 +14,21 @@ const livroSchema = new mongoose.Schema(
     },
     editora: {
       type: String, 
-      required: [true, "O nome da editora é obrigatório."]
+      required: [true, "O nome da editora é obrigatório."],
+      enum: {
+        values: ["Casa do Código", "Alura"],
+        message: "A editora {VALUE} não é um valor permitido"
+      }
     },
-    numeroPaginas: {type: Number}
+    numeroPaginas: {
+      type: Number,
+      validate: {
+        validator: (valor) => {
+          return valor >= 10 && valor <= 5000;
+        },
+        messsage: "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"
+      }
+    }
   }
 );
 
---
+++ src/models/index.js
@@ -0,0 +1,6 @@
+import "./ValidadorGlobal.js";
+import autores from "./Autor.js";
+import livros from "./Livro.js";
+
+export { autores, livros };
+
